fa327cc60ef177d94073e82311bf1e6d2db6606d
Marc Wilson <posguy99@gmail.com>
2020-09-20 20:09:44 -0700

    Initial Commit

---
+++ L2-8_watch_lists.py
@@ -0,0 +1,9 @@
+# L2-8
+# 9-20-20
+
+l = [23, 67, 9, 0, 'Joe']
+print(l)
+l.append(45)
+l.append(75)
+l.pop()
+print(l)
---
+++ L2-9_the_list.py
@@ -0,0 +1,17 @@
+# 2_7
+# 9-20-20
+the_list = []
+the_list.append('345')
+the_list.append(987)
+the_list.append('Python eats the world')
+print(the_list)
+print(the_list[-1])
+print(the_list[-2])
+the_list.pop()
+print(the_list)
+del the_list[1]
+print(the_list)
+the_list.append('Bob')
+print(the_list)
+the_list.remove('Bob')
+print(the_list)
