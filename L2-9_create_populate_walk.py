e5078e7e2cc8d63e7b4d7b644741db88f6d25bea
Marc Wilson <posguy99@gmail.com>
2020-09-20 20:16:12 -0700

    Initial commit

---
+++ L2-9_create_populate_walk.py
@@ -0,0 +1,13 @@
+# 9-20-20
+# l2-9 (except it isn't really since there was already a 2-9)
+
+anotherlist = []
+for item in range(65, 80):
+    anotherlist.append(chr(item))
+for item in range(100,150):
+    anotherlist.append(item)
+print(anotherlist)
+print('*'*20)
+for item in anotherlist:
+    print(item)
+
